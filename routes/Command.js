a67d7ea63f3fac867a335a3011ef21c631b6ee06
yoonun <yoontkprimium@gmail.com>
2024-01-21 22:16:13 +0900

    text fixed

--- routes/Command.js
+++ routes/Command.js
@@ -4,7 +4,7 @@ var action = require('./Action')
 var Command = {
 	start: function(message, chat_id){
 		var msg = {'chat_id': chat_id, 'text':'Hello World!!\nyour text:' +  message}
-		action.sendMessage(chat_id, "Hello World!!\nyour test: " + message)
+		action.sendMessage(chat_id, "Hello World!!\nyour text: " + message)
 	}
 }
 
